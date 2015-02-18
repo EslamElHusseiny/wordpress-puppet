c99bef445950a1191dfcb1ef00888b1a55d347d1
Eslam El Husseiny <eslam.husseiny@gmail.com>
2015-02-18 17:33:39 +0200

    removing mysql service from service.pp

--- manifests/service.pp
+++ manifests/service.pp
@@ -1,11 +1,5 @@
 class wordpress::service{
 
-    service { 'mysql':
-        ensure   => running,
-        enable   => true,
-        provider => smf,
-    }
-
     service { 'apache22':
         ensure => running,
         enable => true,
